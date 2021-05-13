6328d36f0022be6c6639685ababa05da5c0e2d6d
Kıvanç Kamay <kivanc.kamay@gmail.com>
2021-05-13 13:12:05 +0300

    Firebase config file is changed.

--- authentication-firebase/src/firebase/config.js
+++ authentication-firebase/src/firebase/config.js
@@ -3,12 +3,7 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDWH6oyI_AXmLGg9QMtEfaTNV3G2FS2I3U",
-    authDomain: "vue-authentication-ced80.firebaseapp.com",
-    projectId: "vue-authentication-ced80",
-    storageBucket: "vue-authentication-ced80.appspot.com",
-    messagingSenderId: "11221135599",
-    appId: "1:11221135599:web:9cd529caa4d9da02a8ab1a"
+    // INITIALIZE FIREBASE CONFIG OBJECT HERE BEFORE USING THE REPO
 };
 
 firebase.initializeApp(firebaseConfig);
